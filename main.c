f0dad68c3b7d4b4dd9f0e05a601e4b9b20f79968
georgedfilho1 <149505740+georgedfilho1@users.noreply.github.com>
2024-05-28 21:47:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ typedef struct Snakenode {
 } Snakenode;
 
 typedef struct {
-  char Nome[4];
+  char Nome[20];
   int pont;
 } Jogador;
 
@@ -32,106 +32,12 @@ typedef struct Highscore {
   struct Highscore *next;
 } Highscore;
 
-void exibirPlacar(int placar);
-void exibirGameOver();
-void adicionarSnakeNode(Snakenode **head, int x, int y);
-void exibirSnake(Snakenode *head);
-void limparSnake(Snakenode *head);
-void liberarSnake(Snakenode **head);
-void moverSnake(Snakenode **head, int x, int y);
-void exibirMaca(int x, int y);
-int verificarColisaoCorpo(Snakenode *head, int x, int y);
-void gerarProximaMaca(int *x, int *y);
-void ordenarHighscore(Highscore **cabeca, Jogador nick);
-void escreverHighscore(Highscore *cabeca, FILE *fptr);
-void exibirHighscore(Highscore *cabeca);
-void liberarHighscore(Highscore **head);
-
-int main() {
-  Snakenode *head = NULL;
-  int ch = 0, placar = 0, dirX = 1, dirY = 0, tempoIntervalo = TEMPO_INICIAL;
-  FILE *fptr;
-  Jogador player;
-
-  printf("Bem vindo!\nDigite sua sigla de 3 letras: ");
-  scanf("%s", player.Nome);
-  screenInit(1);
-  keyboardInit();
-  timerInit(tempoIntervalo);
-
-  adicionarSnakeNode(&head, 34, 12);
-  srand((unsigned int)time(NULL));
-  int PosMacaX = rand() % 68 + 8, PosMacaY = rand() % 16 + 4;
-  exibirMaca(PosMacaX, PosMacaY);
-  screenUpdate();
-  sleep(3);
-
-  while (ch != KEY_ESC) {
-    if (keyhit()) {
-      ch = readch();
-      switch (ch) {
-        case ARROW_UP: if (dirY != 1) { dirX = 0; dirY = -1; } break;
-        case ARROW_DOWN: if (dirY != -1) { dirX = 0; dirY = 1; } break;
-        case ARROW_LEFT: if (dirX != 1) { dirX = -1; dirY = 0; } break;
-        case ARROW_RIGHT: if (dirX != -1) { dirX = 1; dirY = 0; } break;
-      }
-      screenUpdate();
-    }
-
-    if (timerTimeOver()) {
-      int newX = head->Nodex + dirX, newY = head->Nodey + dirY;
-
-      if (newX >= MAXX || newX <= MINX || newY >= MAXY || newY <= MINY || verificarColisaoCorpo(head, newX, newY)) break;
-
-      if (newX == PosMacaX && newY == PosMacaY) {
-        adicionarSnakeNode(&head, PosMacaX, PosMacaY);
-        gerarProximaMaca(&PosMacaX, &PosMacaY);
-        exibirMaca(PosMacaX, PosMacaY);
-        placar++;
-        tempoIntervalo = (tempoIntervalo > TEMPO_MINIMO) ? (tempoIntervalo - REDUCAO_TEMPO) : TEMPO_MINIMO;
-        timerInit(tempoIntervalo);
-      }
-
-      exibirPlacar(placar);
-      limparSnake(head);
-      moverSnake(&head, newX, newY);
-      exibirSnake(head);
-      screenUpdate();
-    }
-  }
-
-  liberarSnake(&head);
-  keyboardDestroy();
-  screenDestroy();
-  player.pont = placar;
-  fptr = fopen("placar.txt", "a");
-  fwrite(&player, sizeof(Jogador), 1, fptr);
-  fclose(fptr);
-
-  Highscore *lista = NULL;
-  fptr = fopen("placar.txt", "r");
-  while (fread(&player, sizeof(Jogador), 1, fptr) == 1) {
-    ordenarHighscore(&lista, player);
-  }
-  fclose(fptr);
-  fptr = fopen("placar.txt", "w");
-  escreverHighscore(lista, fptr);
-  fclose(fptr);
-  exibirHighscore(lista);
-  liberarHighscore(&lista);
-  exibirGameOver();
-  timerDestroy();
-  return 0;
-}
-
 void exibirGameOver() {
-  screenSetColor(YELLOW, DARKGRAY);
   screenGotoxy(30, 10);
   printf("Game over!");
 }
 
 void exibirPlacar(int placar) {
-  screenSetColor(YELLOW, DARKGRAY);
   screenGotoxy(35, 22);
   printf("Placar : %d", placar);
 }
@@ -248,3 +154,82 @@ void liberarHighscore(Highscore **head) {
     free(temp);
   }
 }
+
+
+int main() {
+  Snakenode *head = NULL;
+  int ch = 0, placar = 0, dirX = 1, dirY = 0, tempoIntervalo = TEMPO_INICIAL;
+  FILE *fptr;
+  Jogador player;
+
+  printf("Bem vindo!\nDigite seu nome: ");
+  scanf("%s", player.Nome);
+  screenInit(1);
+  keyboardInit();
+  timerInit(tempoIntervalo);
+
+  adicionarSnakeNode(&head, 34, 12);
+  srand((unsigned int)time(NULL));
+  int PosMacaX = rand() % 68 + 8, PosMacaY = rand() % 16 + 4;
+  exibirMaca(PosMacaX, PosMacaY);
+  screenUpdate();
+  sleep(3);
+
+  while (ch != KEY_ESC) {
+    if (keyhit()) {
+      ch = readch();
+      switch (ch) {
+        case ARROW_UP: if (dirY != 1) { dirX = 0; dirY = -1; } break;
+        case ARROW_DOWN: if (dirY != -1) { dirX = 0; dirY = 1; } break;
+        case ARROW_LEFT: if (dirX != 1) { dirX = -1; dirY = 0; } break;
+        case ARROW_RIGHT: if (dirX != -1) { dirX = 1; dirY = 0; } break;
+      }
+      screenUpdate();
+    }
+
+    if (timerTimeOver()) {
+      int newX = head->Nodex + dirX, newY = head->Nodey + dirY;
+
+      if (newX >= MAXX || newX <= MINX || newY >= MAXY || newY <= MINY || verificarColisaoCorpo(head, newX, newY)) break;
+
+      if (newX == PosMacaX && newY == PosMacaY) {
+        adicionarSnakeNode(&head, PosMacaX, PosMacaY);
+        gerarProximaMaca(&PosMacaX, &PosMacaY);
+        exibirMaca(PosMacaX, PosMacaY);
+        placar++;
+        tempoIntervalo = (tempoIntervalo > TEMPO_MINIMO) ? (tempoIntervalo - REDUCAO_TEMPO) : TEMPO_MINIMO;
+        timerInit(tempoIntervalo);
+      }
+
+      exibirPlacar(placar);
+      limparSnake(head);
+      moverSnake(&head, newX, newY);
+      exibirSnake(head);
+      screenUpdate();
+    }
+  }
+
+  liberarSnake(&head);
+  keyboardDestroy();
+  screenDestroy();
+  player.pont = placar;
+  fptr = fopen("placar.txt", "a");
+  fwrite(&player, sizeof(Jogador), 1, fptr);
+  fclose(fptr);
+
+  Highscore *lista = NULL;
+  fptr = fopen("placar.txt", "r");
+  while (fread(&player, sizeof(Jogador), 1, fptr) == 1) {
+    ordenarHighscore(&lista, player);
+  }
+  fclose(fptr);
+  fptr = fopen("placar.txt", "w");
+  escreverHighscore(lista, fptr);
+  fclose(fptr);
+  exibirHighscore(lista);
+  liberarHighscore(&lista);
+  exibirGameOver();
+  timerDestroy();
+  return 0;
+}
+
